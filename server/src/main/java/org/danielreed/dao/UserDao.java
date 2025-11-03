7abf706ba61603f7d7d8e71640c6042fc3545292
Daniel Reed <danielreed.dev@gmail.com>
2025-11-03 14:02:08 -0500

    finished user authentication

---
+++ server/src/main/java/org/danielreed/dao/UserDao.java
@@ -0,0 +1,15 @@
+package org.danielreed.dao;
+
+import org.danielreed.model.User;
+import java.util.List;
+
+public interface UserDao {
+
+    List<User> getUsers();
+
+    User getUserById(int userId);
+
+    User getUserByUsername(String username);
+
+    User createUser(User newUser);
+}
---
+++ server/src/main/java/org/danielreed/security/WebSecurityConfig.java
@@ -0,0 +1,71 @@
+package org.danielreed.security;
+
+import org.danielreed.security.jwt.JWTCustomDSL;
+import org.danielreed.security.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final UserModelDetailsService userModelDetailsService;
+
+    public WebSecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler,
+            UserModelDetailsService userModelDetailsService
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.userModelDetailsService = userModelDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(HttpMethod.OPTIONS, "/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler))
+                .sessionManagement( session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/products", "/products/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .with(securityConfigurerAdapter(), Customizer.withDefaults());
+        return http.build();
+    }
+
+    private JWTCustomDSL securityConfigurerAdapter() {
+        return new JWTCustomDSL(tokenProvider);
+    }
+}
