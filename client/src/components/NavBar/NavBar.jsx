c0edf2c2ffa02f1240c500ac5bb3b58ea88115c9
Daniel Reed <danielreed.dev@gmail.com>
2025-12-17 20:11:24 -0500

    added styling to navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -8,19 +8,21 @@ export default function NavBar() {
   const { user } = useContext(UserContext);
 
   return (
-    <nav>
-      <div className={styles.navLeft}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/collection/:userId">Collection</NavLink>
-      </div>
+    <nav className={styles.navBar}>
+      <ul className={styles.navLinks}>
+        <li className={styles.navLeft}>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/collection/:userId">Collection</NavLink>
+        </li>
 
-      <div className={styles.navRight}>
-        {user ? (
-          <Link to="/logout">Logout</Link>
-        ) : (
-          <NavLink to="/login">Login</NavLink>
-        )}
-      </div>
+        <li className={styles.navRight}>
+          {user ? (
+            <Link to="/logout">Logout</Link>
+          ) : (
+            <NavLink to="/login">Login</NavLink>
+          )}
+        </li>
+      </ul>
     </nav>
   );
 }
