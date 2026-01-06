cffd2f0039aed6455e392e85c3555edac9eb9a0d
Daniel Reed <danielreed.dev@gmail.com>
2026-01-06 11:23:08 -0500

    started process of developing collection filter feature. deleted unused backend methods.

--- server/src/main/java/org/danielreed/dao/CollectionDao.java
+++ server/src/main/java/org/danielreed/dao/CollectionDao.java
@@ -12,24 +12,6 @@ public interface CollectionDao {
     // An authenticated user can GET a list of their film collection.
     public List<Film> getCollectionByUserId(int userId);
 
-    // An authenticated user can GET a list of films in their collection by TYPE.
-    public List<Film> getCollectionFilmsByType(String filmType, int userId);
-
-    // An authenticated user can GET a list of films in their collection by GENRE.
-    public List<Film> getCollectionFilmsByGenre(String genre, int userId);
-
-    // An authenticated user can GET a list of films in their collection by RATED.
-    public List<Film> getCollectionFilmsByRated(String rated, int userId);
-
-    // An authenticated user can GET a list of films in their collection by RELEASE YEAR.
-    public List<Film> getCollectionFilmsByReleaseYear(String releaseYear, int userId);
-
-    // An authenticated user can GET a list of films in their collection by ACTOR.
-    public List<Film> getCollectionFilmsByActor(String actors, int userId);
-
-    // An authenticated user can GET a list of films in their collection by DIRECTOR.
-    public List<Film> getCollectionFilmsByDirector(String director, int userId);
-
     // An authenticated user can DELETE a film from their collection.
     public int deleteFilmFromCollection(int userId, String filmId);
 }
